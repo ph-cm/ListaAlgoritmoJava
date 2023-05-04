e6c15c05bd9dd2c01aa9e9aac1d21875f1ab442f
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-03 22:30:56 -0300

    Exercicio 07

---
+++ src/pacote/Ex07.java
@@ -0,0 +1,30 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Ex07 {
+	//Entrar com um ângulo em graus e imprimir: seno, cosseno, tangente, secante, cossecante e cotangente deste ângulo
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double angulo , seno , cosseno , tangente , secante , cossecante , cotangente;
+		
+		System.out.println("Escolha um angulo: ");
+		angulo = sc.nextDouble();
+		
+		seno = Math.sin(angulo);
+		cosseno = Math.cos(angulo);
+		tangente = Math.tan(angulo);
+		secante = 1/cosseno;
+		cossecante = 1/seno;
+		cotangente = 1/tangente;
+		
+		System.out.println("Seno: " + seno);
+		System.out.println("Cosseno: " + cosseno);
+		System.out.println("Tangente: " + tangente);
+		System.out.println("Secante" + secante);
+		System.out.println("Cossecante: " + cossecante);
+		System.out.println("Cotangente: " + cotangente);
+	}
+}
