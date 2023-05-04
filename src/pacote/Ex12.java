c92ee3762666ef45d11cdb371db9f005b25ebe09
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-03 23:50:48 -0300

    Exercicio 12

---
+++ src/pacote/Ex12.java
@@ -0,0 +1,23 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Ex12 {
+/*Criar um algoritmo que leia um valor de hora e informar 
+	quantos minutos se passaram desde o início do dia*/
+	public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int hora , minutos , totalMinutos;
+	
+	System.out.println("Digite a hora atual: ");
+	hora = sc.nextInt();
+	
+	System.out.println("Digite os minutos atuais: ");
+	minutos = sc.nextInt();
+	
+	totalMinutos = (hora * 60) + minutos;
+	System.out.println("Os minutos que passaram do dia são: " + totalMinutos);
+	
+	}
+}
