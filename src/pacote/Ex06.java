25859efa992da74fb1651ccaf711b8c6ebe2154b
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-03 22:15:05 -0300

    Exercicio 06

---
+++ src/pacote/Ex06.java
@@ -0,0 +1,21 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Ex06 {
+	// Ler dois números inteiros e imprimir a soma. Antes do resultado imprimir a mensagem: Soma
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 , n2 , soma;
+		
+		System.out.println("Digite um número:");
+		n1 = sc.nextInt();
+		
+		System.out.println("Digite outro número:");
+		n2 = sc.nextInt();
+		
+		soma = n1 + n2;
+		System.out.println("A soma de " + n1 + " e " + n2 + " é " + soma);
+	}
+}
