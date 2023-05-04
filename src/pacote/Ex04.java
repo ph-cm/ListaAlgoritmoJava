e3f1d91f0c6e44c643028ebf446c96a3c3d7b26d
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-03 22:03:31 -0300

    Exercicio 04

---
+++ src/pacote/Ex04.java
@@ -0,0 +1,18 @@
+package pacote;
+
+public class Ex04 {
+	//Criar um algoritmo que imprima a média aritmética entre os números: 8, 9 e 7.
+	public static void main(String [] argrs) {
+		int n1 , n2 , n3 , media;
+		
+		n1 = 8;
+		n2 = 9;
+		n3 = 7;
+		
+		media = (n1 + n2 + n3)/3;
+		
+		System.out.println("A media aritmetica entre " + n1 + " , " + n2 + " , " + n3 + " é: " + media);
+				
+		
+	}
+}
