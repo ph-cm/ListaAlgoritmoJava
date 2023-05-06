3e7c5be024cf428d30da678bd7adf92282837cc7
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-06 02:35:09 -0300

    Exercicio 17

---
+++ src/pacote/Ex17.java
@@ -0,0 +1,22 @@
+package pacote;
+
+import java.util.Scanner;
+
+// Ler um número e imprimir se ele é par ou ímpar
+public class Ex17 {
+	public static void main (String[]  args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Escolha um número: ");
+		numero = sc.nextInt();
+		
+		if((numero % 2) == 0) {
+			System.out.println("Este número é par");
+		}else {
+			System.out.println("Este número é impar");
+		}
+
+	}
+}
