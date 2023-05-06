233b17112058bf7ea43683fd00697bd1236b6fee
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-06 02:42:41 -0300

    Exercicio 18

---
+++ src/pacote/Ex18.java
@@ -0,0 +1,23 @@
+package pacote;
+
+import java.util.Scanner;
+
+/*Entrar com um número e imprimir uma das mensagens: é múltiplo de 3 ou não é
+múltiplo de 3*/
+public class Ex18 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numeroMultiplo;
+		
+		System.out.println("Escolha um número: ");
+		numeroMultiplo = sc.nextInt();
+		
+		if((numeroMultiplo % 3) == 0 ) {
+			System.out.println("O número é multiplo de 3");
+		}
+		else {
+			System.out.println("O número não é multiplo de 3");
+		}
+	}
+}
