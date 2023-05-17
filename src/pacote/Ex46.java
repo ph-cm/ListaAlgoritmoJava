9a160ba116a0402b50fcc2a69883d5576ff66d6e
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-17 03:25:30 -0300

    Exercicio 46

---
+++ src/pacote/Ex46.java
@@ -0,0 +1,20 @@
+package pacote;
+
+import java.util.Scanner;
+
+//Criar uma função que converta radianos para graus
+public class Ex46 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double radianos , graus;
+		
+		System.out.println("Digite os graus raidanos: ");
+		radianos = sc.nextDouble();
+		
+		graus = radianos * (180 / Math.PI);
+		
+		System.out.print("Em graus é:" + graus);
+		sc.close();
+	}
+}
